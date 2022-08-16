3348798325be5321435a7c42f11181e2f8e4c81f
dwild0 <dwild000@gmail.com>
2022-08-16 13:50:20 +0200

    deleted jupyter files

---
+++ delete_collections.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from arango import ArangoClient
+
+client = ArangoClient(hosts="http://localhost:8529")
+sys_db = client.db("_system", username="root", password="client")
+
+if not sys_db.has_database(("seminar2")):
+    sys_db.create_database(("seminar2"))
+    db = client.db("seminar2", username="root", password="client")
+else:
+    db = client.db("seminar2", username="root", password="client")
+c = 0
+print("connection to database -seminar2- established")
+
+print("List all collections of database -seminar2-")
+print(db.collections())
+
+if db.has_collection("umgebung"):
+    db.delete_collection("umgebung")
+    c += 1
+if db.has_collection("kamera"):
+    db.delete_collection("kamera")
+    c += 1
+if db.has_collection("qual"):
+    db.delete_collection("qual")
+    c += 1
+if db.has_collection("heiz"):
+    db.delete_collection("heiz")
+    c += 1
+if db.has_collection("waage"):
+    db.delete_collection("waage")
+    c += 1
+if db.has_collection("couple"):
+    db.delete_collection("couple")
+    c += 1
+print("\n" +  str(c)+" collections deleted")
